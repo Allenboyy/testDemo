5354693dd8aad5b37743ddede34e6705eb040538
Allenboyy <songxiangmin@163.com>
2018-12-13 23:10:57 +0800

    lianxi tijiao code

---
+++ src/test/java/demo2/Testa2.java
@@ -0,0 +1,14 @@
+package demo2;
+
+public class Testa2 {
+    public static void main(String[] args) {
+        String s1 = "ddddddwfwegwrgg";
+        String s2 = "qfggdgffsdf";
+
+        check(s1,s2);
+    }
+
+    private static boolean check(String s1,String s2){
+        return  false;
+    }
+}
